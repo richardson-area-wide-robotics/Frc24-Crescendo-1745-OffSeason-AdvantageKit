21f1971f0b066747ee4364d30818a91ceef2b585
Tony1547 <7tony10@gmail.com>
2024-10-30 08:55:08 -0500

    maybe works, purple soop

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,6 +5,7 @@
 package frc.robot;
 
 import com.revrobotics.CANSparkFlex;
+import com.revrobotics.CANSparkMax;
 import com.revrobotics.REVPhysicsSim;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
@@ -14,6 +15,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.Constants.PivotConstants;
 import frc.robot.Constants.ShooterConstants.ShooterState;
 import frc.robot.subsystems.Intake.Intake;
 import frc.robot.subsystems.Shooter.Pivot;
@@ -59,11 +61,6 @@ public class RobotContainer {
     SHOOTER_SUBSYSTEM.kickerConfig(new CANSparkFlex(11, MotorType.kBrushless));
     SHOOTER_SUBSYSTEM.shooterConfig(new CANSparkFlex(13, MotorType.kBrushless), false);
     SHOOTER_SUBSYSTEM.shooterConfig(new CANSparkFlex(14, MotorType.kBrushless), true);
-
-
-
-    PIVOT_SUBSYSTEM.pivotConfig(new CANSparkFlex(9, MotorType.kBrushless));
-    PIVOT_SUBSYSTEM.pivotConfig(new CANSparkFlex(10, MotorType.kBrushless));
 
 
 
--- src/main/java/frc/robot/subsystems/Shooter/Pivot.java
+++ src/main/java/frc/robot/subsystems/Shooter/Pivot.java
@@ -8,7 +8,6 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.SparkPIDController;
 import com.revrobotics.CANSparkBase.ControlType;
 import com.revrobotics.CANSparkBase.SoftLimitDirection;
-import com.revrobotics.CANSparkFlex;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.CANSparkLowLevel.PeriodicFrame;
 import edu.wpi.first.units.Angle;
